e71d5ee8ffb032e0ca0dd8061dc7efc02b8c19b9
Abhishek <warriorsshadow39@gmail.com>
2025-02-15 12:15:50 +0530

    done all

--- youngandold.js
+++ youngandold.js
@@ -1,5 +1,26 @@
 import people from './people.js'
 
 function youngandold(data){
+
+    let answer = data.reduce((acc,ele)=>{
+        if(acc[ele.profession]){
+            if(acc[ele.profession]["young"] > ele.age){
+                acc[ele.profession]["young"] = ele.age
+            }
+            else if(acc[ele.profession]["old"] < ele.age){
+                acc[ele.profession]["old"] = ele.age
+            }
+        }
+        else{
+            acc[ele.profession] = {
+                "old" : ele.age,
+                "young":ele.age
+            }
+        }
+        return acc
+    },{})
+    console.log(answer);
     
+
 }
+youngandold(people)
