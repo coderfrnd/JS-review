0053cd7868da5ed35b69af633d618e74dddebd16
Abhishek <warriorsshadow39@gmail.com>
2025-02-15 11:54:08 +0530

    using loops done

--- profession.js
+++ profession.js
@@ -1,23 +1,31 @@
 import people from './people.js'
 
 function profession(data){
-        let answer = data.reduce((acc,ele)=>{
-                  if(acc[ele.profession]){
-                    acc[ele.profession].push(ele.name)
-                  }
-                  else{
-                    acc[ele.profession]=[]
-                    acc[ele.profession].push(ele.name)
-                  }
-                  return acc
-        },{})
-        // console.log(answer);
-        return answer
-
-        // for(let i=0;)
+        // let answer = data.reduce((acc,ele)=>{
+        //           if(acc[ele.profession]){
+        //             acc[ele.profession].push(ele.name)
+        //           }
+        //           else{
+        //             acc[ele.profession]=[]
+        //             acc[ele.profession].push(ele.name)
+        //           }
+        //           return acc
+        // },{})
+        // // console.log(answer);
+        // return answer
+let answer ={}
+        for(let i=0; i<data.length;i++){
+               if(answer[data[i].profession]){
+                answer[data[i].profession].push(data[i].name)
+               }
+               else{
+                answer[data[i].profession] = []
+                answer[data[i].profession].push(data[i].name)
+               }
+        }
+        console.log(answer)
         
 }
-
-let answer = profession(people)
-console.log(answer);
+ profession(people)
+// console.log(answer);
 
